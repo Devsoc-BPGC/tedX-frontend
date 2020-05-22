1e330f037dc0ad8586e2d2a03e33b00fdc1f95a8
kanishq1 <kanishqkhandelwal@gmail.com>
2020-05-23 03:07:51 +0530

    linked speaker and blog page with backend

---
+++ assets/js/blog.js
@@ -0,0 +1,88 @@
+const createBlog = (photo, title, author, blogid, content) => {
+    console.log("!");
+    const article = document.createElement("article");
+    article.setAttribute("class", "entry");
+    article.setAttribute("data-aos", "fade-up");
+
+    const body = document.querySelector("#blog-content");
+    body.append(article);
+
+    const div1 = document.createElement("div");
+    div1.classList.add('entry-img');
+    article.append(div1);
+
+    const div2 = document.createElement("div");
+    div2.classList.add('blog-image');
+    div2.setAttribute("style", `background-image: url('${photo}');`);
+    div1.append(div2);
+
+    const h2 = document.createElement("h2");
+    h2.classList.add('entry-title');
+    article.append(h2);
+
+    const a = document.createElement("a");
+    a.classList.add('blog-content-title');
+    a.setAttribute("href", "blog-single.html");
+    h2.append(a);
+    a.innerHTML = title;
+
+    const div3 = document.createElement("div");
+    div3.classList.add('entry-meta');
+    article.append(div3);
+
+    const ul = document.createElement("ul");
+    div3.append(ul);
+
+    const li1 = document.createElement("li");
+    li1.classList.add('d-flex');
+    li1.classList.add('align-items-center');
+    li1.innerHTML = `<i class="icofont-user"></i>`
+    ul.append(li1);
+
+    const a1 = document.createElement("a");
+    a1.classList.add('blog-content-author');
+    a1.setAttribute("href", "blog-single.html");
+    a1.innerHTML = author;
+    li1.append(a1);
+
+
+    const div4 = document.createElement("div");
+    div4.classList.add('entry-content');
+    article.append(div4);
+
+    const p = document.createElement("p");
+    p.classList.add('blog-content');
+    let introContent = content.slice(0, 400);
+    p.innerHTML = introContent + " . . . .";
+    div4.append(p);
+
+    const div5 = document.createElement("div");
+    div5.classList.add('read-more');
+    div4.append(div5);
+
+    const a3 = document.createElement("a");
+    a3.setAttribute("href", `blog-single.html?blogId=${blogid}`);
+    a3.innerHTML = `Read More`
+    div5.append(a3);
+
+}
+
+const getBlogs = () => {
+    axios
+        .get(
+            "https://tedxbackend.herokuapp.com/api/blogs"
+        )
+        .then((response) => {
+            for (let index = 0; index < response.data.data.length; index++) {
+                let photo = response.data.data[0].imageURL;
+                console.log(response);
+                let title = response.data.data[0].title;
+                let author = response.data.data[0].author;
+                let content = response.data.data[0].content;
+                let blogid = response.data.data[0]._id;
+                createBlog(photo, title, author, blogid, content);
+            }
+        });
+};
+
+getBlogs();
--- assets/js/data.js
+++ assets/js/data.js
@@ -1,130 +1,130 @@
 //add speakers data in bellow array
 
-const data = [{
-    id: 1,
-    pic: "./assets/speakers/2.jpg",
-    name: "Elli Avrram",
-    info: "Year 2010"
-  },
-  {
-    id: 2,
-    pic: "./assets/speakers/5.jpg",
-    name: "Viraj Ghelani",
-    info: "Year 2020"
-  },
-  {
-    id: 3,
-    pic: "./assets/speakers/18.jpg",
-    name: "Kirti Kulhari",
-    info: "Year 2019"
-  },
-  {
-    id: 4,
-    pic: "./assets/speakers/15.jpg",
-    name: "Naveen Polishetty",
-    info: "Year 2018"
-  },
-  {
-    id: 5,
-    pic: "./assets/speakers/12.jpg",
-    name: "Sumeet Vyas",
-    info: "Year 2017"
-  },
-  {
-    id: 6,
-    pic: "./assets/speakers/11.jpg",
-    name: "Piyush Mishra",
-    info: "Year 2017"
-  },
-  {
-    id: 7,
-    pic: "./assets/speakers/16.jpg",
-    name: "Kajal Agarwal",
-    info: "Year 2018"
-  },
-  {
-    id: 8,
-    pic: "./assets/speakers/14.jpg",
-    name: "Aadar Malik",
-    info: "Year 2018"
-  },
-  {
-    id: 9,
-    pic: "./assets/speakers/9.jpg",
-    name: "Priya Mishra",
-    info: "Year 2020"
-  },
-  {
-    id: 10,
-    pic: "./assets/speakers/10.jpg",
-    name: "Sudhir R",
-    info: "Year 2020"
-  },
-  {
-    id: 11,
-    pic: "./assets/speakers/11.jpg",
-    name: "Arpit",
-    info: "Year 2020"
-  },
-  {
-    id: 12,
-    pic: "./assets/speakers/12.jpg",
-    name: "Arpit",
-    info: "Year 2020"
-  },
-  {
-    id: 13,
-    pic: "./assets/speakers/13.jpg",
-    name: "Arpit",
-    info: "Year 2020"
-  },
-  {
-    id: 14,
-    pic: "./assets/speakers/14.jpg",
-    name: "Arpit",
-    info: "Year 2020"
-  },
-  {
-    id: 15,
-    pic: "./assets/speakers/15.jpg",
-    name: "Arpit",
-    info: "Year 2020"
-  },
-  {
-    id: 16,
-    pic: "./assets/speakers/16.jpg",
-    name: "Arpit",
-    info: "jdata"
-  },
-  {
-    id: 17,
-    pic: "./assets/speakers/17.jpg",
-    name: "Arpit",
-    info: "jdata"
-  },
-  {
-    id: 18,
-    pic: "./assets/speakers/18.jpg",
-    name: "Arpit",
-    info: "jdata"
-  },
-  {
-    id: 19,
-    pic: "./assets/speakers/19.jpg",
-    name: "Arpit",
-    info: "jdata"
-  },
-  {
-    id: 204,
-    pic: "./assets/speakers/20.jpg",
-    name: "Padmashri Saravanan",
-    info: "Year 2020"
-  }
-]
+// const data = [{
+//   id: 1,
+//   pic: "./assets/speakers/2.jpg",
+//   name: "Elli Avrram",
+//   info: "Year 2010"
+// },
+// {
+//   id: 2,
+//   pic: "./assets/speakers/5.jpg",
+//   name: "Viraj Ghelani",
+//   info: "Year 2020"
+// },
+// {
+//   id: 3,
+//   pic: "./assets/speakers/18.jpg",
+//   name: "Kirti Kulhari",
+//   info: "Year 2019"
+// },
+// {
+//   id: 4,
+//   pic: "./assets/speakers/15.jpg",
+//   name: "Naveen Polishetty",
+//   info: "Year 2018"
+// },
+// {
+//   id: 5,
+//   pic: "./assets/speakers/12.jpg",
+//   name: "Sumeet Vyas",
+//   info: "Year 2017"
+// },
+// {
+//   id: 6,
+//   pic: "./assets/speakers/11.jpg",
+//   name: "Piyush Mishra",
+//   info: "Year 2017"
+// },
+// {
+//   id: 7,
+//   pic: "./assets/speakers/16.jpg",
+//   name: "Kajal Agarwal",
+//   info: "Year 2018"
+// },
+// {
+//   id: 8,
+//   pic: "./assets/speakers/14.jpg",
+//   name: "Aadar Malik",
+//   info: "Year 2018"
+// },
+// {
+//   id: 9,
+//   pic: "./assets/speakers/9.jpg",
+//   name: "Priya Mishra",
+//   info: "Year 2020"
+// },
+// {
+//   id: 10,
+//   pic: "./assets/speakers/10.jpg",
+//   name: "Sudhir R",
+//   info: "Year 2020"
+// },
+// {
+//   id: 11,
+//   pic: "./assets/speakers/11.jpg",
+//   name: "Arpit",
+//   info: "Year 2020"
+// },
+// {
+//   id: 12,
+//   pic: "./assets/speakers/12.jpg",
+//   name: "Arpit",
+//   info: "Year 2020"
+// },
+// {
+//   id: 13,
+//   pic: "./assets/speakers/13.jpg",
+//   name: "Arpit",
+//   info: "Year 2020"
+// },
+// {
+//   id: 14,
+//   pic: "./assets/speakers/14.jpg",
+//   name: "Arpit",
+//   info: "Year 2020"
+// },
+// {
+//   id: 15,
+//   pic: "./assets/speakers/15.jpg",
+//   name: "Arpit",
+//   info: "Year 2020"
+// },
+// {
+//   id: 16,
+//   pic: "./assets/speakers/16.jpg",
+//   name: "Arpit",
+//   info: "jdata"
+// },
+// {
+//   id: 17,
+//   pic: "./assets/speakers/17.jpg",
+//   name: "Arpit",
+//   info: "jdata"
+// },
+// {
+//   id: 18,
+//   pic: "./assets/speakers/18.jpg",
+//   name: "Arpit",
+//   info: "jdata"
+// },
+// {
+//   id: 19,
+//   pic: "./assets/speakers/19.jpg",
+//   name: "Arpit",
+//   info: "jdata"
+// },
+// {
+//   id: 204,
+//   pic: "./assets/speakers/20.jpg",
+//   name: "Padmashri Saravanan",
+//   info: "Year 2020"
+// }
+// ]
 
 
 //the bellow code will add a new speaker card for every speaker in speakers array
-const buildspeakerCard = (speaker, component) => {
+const buildspeakerCard = (name, photo, info, component) => {
   console.log(1);
   const div = document.createElement("div");
   div.setAttribute("class", "col-lg-3");
@@ -153,7 +153,7 @@ const buildspeakerCard = (speaker, component) => {
 
   const a = document.createElement("a");
   div3.append(a);
-  a.setAttribute("href", speaker.name + ".html");
+  a.setAttribute("href", name + ".html");
   a.innerHTML = "Visit Speaker Page";
 
   const div4 = document.createElement("div");
@@ -166,20 +166,33 @@ const buildspeakerCard = (speaker, component) => {
   const span = document.createElement("span");
   div4.append(span);
 
-  img.setAttribute("src", speaker.pic);
-  h4.innerHTML = speaker.name;
-  span.innerHTML = speaker.info;
+  img.setAttribute("src", photo);
+  h4.innerHTML = name;
+  span.innerHTML = "Year " + info;
 
 }
 
-if (document.getElementsByClassName("row1").length) {
-  data.forEach(speaker => buildspeakerCard(speaker, ".row1"));
-} else
-  data.slice(0, 8).forEach(speaker => buildspeakerCard(speaker, ".rowHome"));
+// if (document.getElementsByClassName("row1").length) {
+//   data.forEach(speaker => buildspeakerCard(speaker, ".row1"));
+// } else
+//   data.slice(0, 8).forEach(speaker => buildspeakerCard(speaker, ".rowHome"));
 
 
+const getSpeakers = () => {
+  axios
+    .get(
+      "https://tedxbackend.herokuapp.com/api/speakers"
+    )
+    .then((response) => {
+      for (let index = 0; index < response.data.data.length; index++) {
+        let photo = response.data.data[0].imageURL;
+        console.log(response);
+        let name = response.data.data[0].name;
+        let year = response.data.data[0].year;
+        buildspeakerCard(name, photo, year, ".row1");
+      }
+    });
+};
 
-// function fu() {
-//   var opened = window.open("");
-//   opened.document.write("<html><head><title>MyTitle</title></head><body>test</body></html>);
-// }
+getSpeakers();
+
