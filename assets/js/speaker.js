9e151c987f74a50533da88a40df420c7797394f4
Arpit Bhardwaj <f20180075@goa.bits-pilani.ac.in>
2020-05-24 21:37:29 +0530

    Initial commit

---
+++ assets/js/speaker.js
@@ -0,0 +1,17 @@
+const id = window.location.href.split('?')[1].split('=')[1];
+
+const link = 'https://tedxbackend.herokuapp.com/api/speakers/' + id;
+console.log(link);
+
+axios
+    .get(
+      link
+    )
+    .then((response) => {
+      console.log(response.data.data);
+      document.getElementById('title').innerHTML = response.data.data.name;
+      document.getElementById('profile-pic').setAttribute("src", response.data.data.imageURL[0]);
+      document.getElementById('about-content').innerHTML = response.data.data.description;
+      document.getElementById('year').innerHTML = response.data.data.year;
+      document.title = response.data.data.name + " - TEDx BITSGoa";
+    });
